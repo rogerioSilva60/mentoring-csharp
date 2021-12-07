050421002fd261294950413f727d606af4364718
Rogerio Araujo <Rogerio_Araujo@Dellteam.com>
2021-12-07 19:03:09 -0300

    new changes

--- EFCore.WebAPI/Models/Battle.cs
+++ EFCore.WebAPI/Models/Battle.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EFCore.WebAPI.Models
 {
+    [Table("battle")]
     public class Battle
     {
         public int Id { get; set; }
+        [Column("name")]
         public string  Name { get; set; }
         public string Description { get; set; }
+        [Column("start_date")]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public List<HeroBattle> HeroBattles { get; set; }
